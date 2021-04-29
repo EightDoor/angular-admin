ff708f2bc9f80baa29fbd62edeab18cfb06c8783
EightDoor <zhou851708184@163.com>
2021-04-29 23:34:32 +0800

    chro: eslint -> eslint装换

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 # 依赖
 
 - ng-alain https://ng-alain.com/
+- angular tslint 转换为eslint https://github.com/angular-eslint/angular-eslint
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,7 +11,7 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 registerLocaleData(zh);
 
 @NgModule({
